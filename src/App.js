9989e52eb0a839481fa1408b4574869915d2d08d
Tim La <tlamarketing111@gmail.com>
2022-08-02 13:18:28 -0400

    utilized props to create multiple users

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <User />
+      <User name="Pedro" age={21} email="pedro@gmail.com" />
+      <User name="Tim" age={29} email="tim@gmail.com" />
     </div>
   );
 }
 
-const User = () => {
+const User = (props) => {
   return (
     <div>
-      <h1>Pedro</h1>
-      <h1>21</h1>
-      <h1>Pedro@gmail.com</h1>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
     </div>
   )
 }
