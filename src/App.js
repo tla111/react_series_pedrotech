5a2e7eff4da3e44c96acaaca2a7e99d8e0b32055
Tim La <tlamarketing111@gmail.com>
2022-08-02 13:15:12 -0400

    concising code by putting data in variable

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
 
 function App() {
+  const name = <h2>Pedro</h2>
+  const age = <h3>21</h3>
+  const email = <h4>Pedro@pedro.com</h4>
+  const user = (
+    <div>
+      {name}
+      {age}
+      {email}
+    </div>
+  )
   return (
-    <div className="App">Hello World</div>
+    <div className="App">
+      {user}
+    </div>
   );
 }
 
