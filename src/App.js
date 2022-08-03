60ae75b051452bc47b4bb62497e96a6226838413
Tim La <tlamarketing111@gmail.com>
2022-08-03 19:23:19 -0400

    created new component to practice props

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <User name="Pedro" age={21} email="pedro@gmail.com" />
       <User name="Tim" age={29} email="tim@gmail.com" />
+      <AppleStore product="iPhone" quantity="1" price="500" />
     </div>
   );
 }
@@ -18,5 +19,13 @@ const User = ({ name, age, email }) => {
     </div>
   )
 }
+
+const AppleStore = ({ product, quantity, price }) => (
+  <div>
+    <h1>{product}</h1>
+    <h1>{quantity}</h1>
+    <h1>${price}</h1>
+  </div>
+)
 
 export default App;
