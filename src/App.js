252668daf458d06340f785e3da2ef268a929e67b
Tim La <tlamarketing111@gmail.com>
2022-08-06 10:40:33 -0400

    imported car component to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import AppleStore from './Components/AppleStore';
 import User from './Components/User';
+import Car from './Components/Car';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <User name="Pedro" age={21} email="pedro@gmail.com" />
       <User name="Tim" age={29} email="tim@gmail.com" />
       <AppleStore product="iPhone" quantity="1" price="500" />
+      <Car />
     </div>
   );
 }
