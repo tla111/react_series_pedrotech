81f20f5b191d7e3dde294c848d307d2bd7b681a9
Tim La <tlamarketing111@gmail.com>
2022-08-10 05:37:45 -0400

    created todoList state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+    const [todoList, setTodoList] = useState([]);
+
     return (
         <div className="App">
             <div className="addTask">
