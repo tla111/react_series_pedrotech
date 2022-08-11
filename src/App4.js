0caef6f8aae31b3ce0d8c3f214a0c6ca8ce6cbac
Tim La <tlamarketing111@gmail.com>
2022-08-11 05:54:05 -0400

    created fourth app component

---
+++ src/App4.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">App4</div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 // import App from './App';
 // import App1 from './App1';
 // import App2 from './App2';
-import App3 from './App3';
+// import App3 from './App3';
+import App4 from './App4';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App3 />
+    <App4 />
   </React.StrictMode>
 );
 
