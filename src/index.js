7e936baa2ce67fa164d6326c6215003a78dec097
Tim La <tlamarketing111@gmail.com>
2022-08-11 05:52:06 -0400

    commented out unused imported app component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import App1 from './App1';
-import App2 from './App2';
+// import App from './App';
+// import App1 from './App1';
+// import App2 from './App2';
 import App3 from './App3';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
