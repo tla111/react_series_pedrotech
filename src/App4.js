575f41a6d77b6a70066d6224597a7c71b61df946
Tim La <tlamarketing111@gmail.com>
2022-08-13 04:36:29 -0400

    onchange to store name

--- src/App4.js
+++ src/App4.js
@@ -4,19 +4,19 @@ import Axios from 'axios';
 
 
 const App = () => {
-    const [age, setAge] = useState("")
+    const [name, setName] = useState("")
 
     const fetchData = () => {
         Axios.get("https://api.agify.io/?name=pedro")
-            .then((res) => setAge(res.data.age));
+            .then((res) => setName(res.data.age));
     }
 
     return (
         <div className="App">
-            <input placeholder="Ex. Pedro..." />
+            <input placeholder="Ex. Pedro..." onChange={(e) => setName(e.target.value)} />
             <button onClick={fetchData}>Predict Age</button>
 
-            <h1>Predicted Age: {age}</h1>
+            <h1>Predicted Age: {name}</h1>
         </div>
     )
 }
