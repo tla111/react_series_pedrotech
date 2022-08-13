43e5fbe3d96a7d28a5c3b2d0dc62094257a8f55f
Tim La <tlamarketing111@gmail.com>
2022-08-12 21:58:28 -0400

    increaseScore function increments score by 1

--- src/App4.js
+++ src/App4.js
@@ -3,10 +3,15 @@ import './App.css';
 
 const App = () => {
     const [score, setScore] = useState(0)
+
+    const increaseScore = () => {
+        setScore(score + 1);
+    }
     return (
         <div className="App">
             <h1>App</h1>
             <h2>{score}</h2>
+            <button onClick={increaseScore}>Add Goal</button>
         </div>
     )
 }
