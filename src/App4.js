1de4278c4fbb46a3c920941142b58302d6959457
Tim La <tlamarketing111@gmail.com>
2022-08-13 04:15:20 -0400

    delete practice code in app4

--- src/App4.js
+++ src/App4.js
@@ -2,21 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-    const [score, setScore] = useState(0)
-
-    const increaseScore = () => {
-        setScore(score + 1);
-    }
-    const decreaseScore = () => {
-        setScore(score - 1);
-    }
     return (
-        <div className="App">
-            <h1>App</h1>
-            <h2>{score}</h2>
-            <button onClick={increaseScore}>Add Goal</button>
-            <button onClick={decreaseScore}>Deduct Goal</button>
-        </div>
+        <div></div>
     )
 }
 
