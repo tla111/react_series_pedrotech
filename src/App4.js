efd12d9d6b64e10e16a84dc3cbf8fd64fa082ce2
Tim La <tlamarketing111@gmail.com>
2022-08-13 04:42:52 -0400

    use object to display data

--- src/App4.js
+++ src/App4.js
@@ -5,11 +5,11 @@ import Axios from 'axios';
 
 const App = () => {
     const [name, setName] = useState("");
-    const [predictedAge, setPredictedAge] = useState(0);
+    const [predictedAge, setPredictedAge] = useState(null);
 
     const fetchData = () => {
         Axios.get(`https://api.agify.io/?name=${name}`)
-            .then((res) => setPredictedAge(res.data.age));
+            .then((res) => setPredictedAge(res.data));
     }
 
     return (
@@ -17,7 +17,9 @@ const App = () => {
             <input placeholder="Ex. Pedro..." onChange={(e) => setName(e.target.value)} />
             <button onClick={fetchData}>Predict Age</button>
 
-            <h1>Predicted Age: {predictedAge}</h1>
+            <h1>Name: {predictedAge?.name}</h1>
+            <h1>Predicted Age: {predictedAge?.age}</h1>
+            <h1>Count: {predictedAge?.count}</h1>
         </div>
     )
 }
