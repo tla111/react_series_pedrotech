a3a3ae767e34a6cf8b310931940088445b3b755c
Tim La <tlamarketing111@gmail.com>
2022-08-13 04:19:56 -0400

    used fetch to grab cat data

--- src/App4.js
+++ src/App4.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+fetch("https://catfact.ninja/fact")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
 const App = () => {
     return (
         <div className="App">
