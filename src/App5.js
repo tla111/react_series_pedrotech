801e8ba9b03c40142db261b9a687e58a6c343ebd
Tim La <tlamarketing111@gmail.com>
2022-08-13 04:45:34 -0400

    created app5

---
+++ src/App5.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App5 = () => {
+    return (
+        <div className="App">App5</div>
+    )
+}
+
+export default App5;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom/client';
 // import App1 from './App1';
 // import App2 from './App2';
 // import App3 from './App3';
-import App4 from './App4';
+// import App4 from './App4';
+import App5 from './App5';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App4 />
+    <App5 />
   </React.StrictMode>
 );
 
