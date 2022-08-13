d6618ae103f49d9242ef0b8e592dbb5d20f593f3
Tim La <tlamarketing111@gmail.com>
2022-08-13 04:22:03 -0400

    use axios to fetch cat fact

--- src/App4.js
+++ src/App4.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 fetch("https://catfact.ninja/fact")
     .then((res) => res.json())
     .then((data) => console.log(data));
+
+Axios.get("https://catfact.ninja/fact").then((res) => { console.log("Axios", res.data); })
 
 const App = () => {
     return (
