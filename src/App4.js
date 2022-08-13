9b4711aebb4fe66cbee79763516040da99895754
Tim La <tlamarketing111@gmail.com>
2022-08-13 04:31:15 -0400

    new axios tutorial

--- src/App4.js
+++ src/App4.js
@@ -4,22 +4,11 @@ import Axios from 'axios';
 
 
 const App = () => {
-    const [catFact, setCatFact] = useState("");
-
-    const fetchCatFact = () => {
-        Axios.get("https://catfact.ninja/fact").then((res) => {
-            setCatFact(res.data.fact);
-        })
-    }
-
-    useEffect(() => {
-        fetchCatFact()
-    }, []);
 
     return (
         <div className="App">
-            <button onClick={fetchCatFact}>Generate Cat Fact</button>
-            <p>{catFact}</p>
+            <input />
+            <button></button>
         </div>
     )
 }
