e04b7192225a9943a4acec7c791fdac31dc959b6
Tim La <tlamarketing111@gmail.com>
2022-08-13 04:25:38 -0400

    use useEffect to display cat fact data once on page

--- src/App4.js
+++ src/App4.js
@@ -6,9 +6,12 @@ import Axios from 'axios';
 const App = () => {
     const [catFact, setCatFact] = useState("");
 
-    Axios.get("https://catfact.ninja/fact").then((res) => {
-        setCatFact("Axios", res.data);
-    })
+    useEffect(() => {
+        Axios.get("https://catfact.ninja/fact").then((res) => {
+            setCatFact(res.data.fact);
+        })
+    }, []);
+
     return (
         <div className="App">
             <button>Generate Cat Fact</button>
