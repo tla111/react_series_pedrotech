4c37fda9ba5d0faed65d5dee74a7616d23e5e2bd
Tim La <tlamarketing111@gmail.com>
2022-08-12 21:57:08 -0400

    created state for score

--- src/App4.js
+++ src/App4.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+    const [score, setScore] = useState(0)
     return (
         <div className="App">
             <h1>App</h1>
+            <h2>{score}</h2>
         </div>
     )
 }
