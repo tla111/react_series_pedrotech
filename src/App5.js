705d5d391e446010ede27095d3b1115e416a1091
Tim La <tlamarketing111@gmail.com>
2022-08-15 07:08:56 -0400

    created menu & contact page

--- src/App5.js
+++ src/App5.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Pages/Home";
+import Menu from './Pages/Menu';
+import Contact from './Pages/Contact';
 
 const App5 = () => {
 
@@ -10,6 +12,8 @@ const App5 = () => {
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/menu" element={<Menu />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
             </Router>
         </div>
