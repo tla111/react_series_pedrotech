98083e99c377786982ac2188917aceb4f5818f57
Tim La <tlamarketing111@gmail.com>
2022-08-15 07:11:54 -0400

    able to route to different pages

--- src/App5.js
+++ src/App5.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from "./Pages/Home";
 import Menu from './Pages/Menu';
 import Contact from './Pages/Contact';
@@ -10,6 +10,11 @@ const App5 = () => {
     return (
         <div className="App">
             <Router>
+                <div>
+                    <Link to="/">Home</Link>
+                    <Link to="/menu">Menu</Link>
+                    <Link to="/contact">Contact</Link>
+                </div>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/menu" element={<Menu />} />
