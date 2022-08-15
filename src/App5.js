eada245575d60a8605aab98afb15496ef748cd71
Tim La <tlamarketing111@gmail.com>
2022-08-15 06:22:08 -0400

    removed practice code

--- src/App5.js
+++ src/App5.js
@@ -1,19 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Axios from 'axios';
+
 
 const App5 = () => {
-    const [nameData, setNameData] = useState({});
-
-    useEffect(() => {
-        Axios.get("https://api.agify.io/?name=Tim").then((res) => setNameData(res.data));
-    }, [])
 
 
     return (
         <div className="App">
             <h1>App5</h1>
-            {nameData.name} {nameData.age}
         </div>
     )
 }
