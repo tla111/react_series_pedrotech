be19b629539395344f24b2543aebd1a318697c6f
Tim La <tlamarketing111@gmail.com>
2022-08-16 11:39:47 -0400

    contact pagehas state usernmame using useContext

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { AppContext } from '../App5';
 
 const Contact = () => {
+    const { username } = useContext(AppContext)
     return (
         <div>
-            <h1>THIS IS THE CONTACT PAGE</h1>
+            <h1>THIS IS THE CONTACT PAGE {username}</h1>
         </div>
     )
 }
 
-export default Contact
+export default Contact;
