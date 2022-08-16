4a552955f24c5dde15642ebf6944f979dd0750c9
Tim La <tlamarketing111@gmail.com>
2022-08-16 11:11:30 -0400

    using app5 for new tutorial

--- src/App5.js
+++ src/App5.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Pages/Home";
 import Menu from './Pages/Menu';
 import Contact from './Pages/Contact';
