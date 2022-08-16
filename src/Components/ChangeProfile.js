d9895a80b3093fcb50161839d615d9381860f16a
Tim La <tlamarketing111@gmail.com>
2022-08-16 11:24:22 -0400

    logic created for ChangeProfile component

--- src/Components/ChangeProfile.js
+++ src/Components/ChangeProfile.js
@@ -1,1 +1,15 @@
-export const ChangeProfile = () => { };
+import React, { useState } from 'react';
+
+const ChangeProfile = () => {
+    const [newUsername, setNewUsername] = useState("");
+    return (
+        <div>
+            {""}
+            <input onChange={(e) => setNewUsername(e.target.value)} />
+            <button onClick={ }>Change Username</button>
+        </div>
+    )
+
+}
+
+export default ChangeProfile
