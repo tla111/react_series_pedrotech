1ec52fffe53452304f6ee7fa857c87266423fd19
Tim La <tlamarketing111@gmail.com>
2022-08-16 11:12:38 -0400

    removed menu page

--- src/App5.js
+++ src/App5.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Pages/Home";
-import Menu from './Pages/Menu';
 import Contact from './Pages/Contact';
 import Navbar from './Components/Navbar';
 
@@ -14,7 +13,6 @@ const App5 = () => {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/menu" element={<Menu />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
                 </Routes>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,6 @@ const Navbar = () => {
     return (
         <div>
             <Link to="/">Home</Link>
-            <Link to="/menu">Menu</Link>
             <Link to="/contact">Contact</Link>
         </div>
     )
