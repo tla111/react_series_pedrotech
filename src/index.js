781149f2768cdd509523a0d729d03b9a1b839a11
Tim La <tlamarketing111@gmail.com>
2022-08-16 11:40:21 -0400

    uncommented App6

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import ReactDOM from 'react-dom/client';
 // import App2 from './App2';
 // import App3 from './App3';
 // import App4 from './App4';
-import App5 from './App5';
-// import App6 from './App6';
+// import App5 from './App5';
+import App6 from './App6';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App5 />
+    <App6 />
   </React.StrictMode>
 );
 
