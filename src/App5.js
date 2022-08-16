9ab4cbfb331b72af6b19539772b1db431d4554d5
Tim La <tlamarketing111@gmail.com>
2022-08-16 11:31:14 -0400

    imported & using useContext

--- src/App5.js
+++ src/App5.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Pages/Home";
@@ -6,19 +6,23 @@ import Profile from './Pages/Profile';
 import Contact from './Pages/Contact';
 import Navbar from './Components/Navbar';
 
+export const AppContext = createContext();
+
 const App5 = () => {
     const [username, setUsername] = useState("PedroTech");
     return (
         <div className="App">
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home username={username} />} />
-                    <Route path="/profile" element={<Profile username={username} setUsername={setUsername} />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
-                </Routes>
-            </Router>
+            <AppContext.Provider>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home username={username} />} />
+                        <Route path="/profile" element={<Profile username={username} setUsername={setUsername} />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+                    </Routes>
+                </Router>
+            </AppContext.Provider>
         </div>
     )
 }
