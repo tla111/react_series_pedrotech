407e92561ba62c669cec9ba12cdcc2a378620813
Tim La <tlamarketing111@gmail.com>
2022-08-16 11:19:23 -0400

    created username state & added the states as props  to components

--- src/App5.js
+++ src/App5.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Pages/Home";
@@ -7,14 +7,14 @@ import Contact from './Pages/Contact';
 import Navbar from './Components/Navbar';
 
 const App5 = () => {
-
+    const [username, setUsername] = useState("");
     return (
         <div className="App">
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/" element={<Home username={username} />} />
+                    <Route path="/profile" element={<Profile username={username} />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
                 </Routes>
