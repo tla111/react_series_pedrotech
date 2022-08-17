40b961410df0d2b11ec66c60e3ca492969fa085a
Tim La <tlamarketing111@gmail.com>
2022-08-17 06:10:22 -0400

    wrapped component with QueryClientProvider

--- src/App5.js
+++ src/App5.js
@@ -5,24 +5,29 @@ import Home from "./Pages/Home";
 import Profile from './Pages/Profile';
 import Contact from './Pages/Contact';
 import Navbar from './Components/Navbar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export const AppContext = createContext();
 
 const App5 = () => {
+    const client = new QueryClient();
+
     const [username, setUsername] = useState("PedroTech");
     return (
         <div className="App">
-            <AppContext.Provider value={{ username, setUsername }}>
-                <Router>
-                    <Navbar />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
-                    </Routes>
-                </Router>
-            </AppContext.Provider>
+            <QueryClientProvider client={client}>
+                <AppContext.Provider value={{ username, setUsername }}>
+                    <Router>
+                        <Navbar />
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/profile" element={<Profile />} />
+                            <Route path="/contact" element={<Contact />} />
+                            <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+                        </Routes>
+                    </Router>
+                </AppContext.Provider>
+            </QueryClientProvider>
         </div>
     )
 }
