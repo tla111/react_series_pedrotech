d2ed4f9b9c0f0b4160be32a16cedcbe4922c2823
Tim La <tlamarketing111@gmail.com>
2022-08-17 06:16:28 -0400

    experimenting with isLoading & isErro

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,14 +5,18 @@ import { useQuery } from '@tanstack/react-query';
 
 const Home = () => {
     const { username } = useContext(AppContext);
-    const { data } = useQuery(["cat"], () => {
+    const { data, isLoading, isError } = useQuery(["cat"], () => {
         return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
     });
+
+    if (isError) {
+        return <h1>...Error</h1>
+    }
 
     return (
         <div>
             <h1>THIS IS THE HOMEPAGE & user is: {username}</h1>
-            <p>{data.fact}</p>
+            <p>{data?.fact}</p>
         </div>
     )
 }
