2aa9b9615c2f73fc1ee74e6f3b975e053a26ee45
Tim La <tlamarketing111@gmail.com>
2022-08-17 06:17:39 -0400

    load new fact with reFetch

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,18 +5,19 @@ import { useQuery } from '@tanstack/react-query';
 
 const Home = () => {
     const { username } = useContext(AppContext);
-    const { data, isLoading, isError } = useQuery(["cat"], () => {
+    const { data, isLoading, isError, refetch } = useQuery(["cat"], () => {
         return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
     });
 
-    if (isError) {
-        return <h1>...Error</h1>
-    }
+    // if (isError) {
+    //     return <h1>...Error</h1>
+    // }
 
     return (
         <div>
             <h1>THIS IS THE HOMEPAGE & user is: {username}</h1>
             <p>{data?.fact}</p>
+            <button onClick={refetch}>Refresh</button>
         </div>
     )
 }
