ea90f64e28e1101f7f5351529de8b21f1d191eda
Tim La <tlamarketing111@gmail.com>
2022-08-18 11:59:08 -0400

    modified error message for confirmPassword

--- src/App6.js
+++ src/App6.js
@@ -10,7 +10,7 @@ const App6 = () => {
         email: yup.string().email().required(),
         age: yup.number().positive().integer().min(18).required(),
         password: yup.string().min(4).max(20).required(),
-        confirmPassword: yup.string().oneOf([yup.ref("password"), null]).required()
+        confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Passwords Don't Match").required()
     })
 
     const { register, handleSubmit, formState: { errors } } = useForm({
