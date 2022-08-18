04f75ea9d6f5c7b888d51e6e621eef4e0074b1fd
Tim La <tlamarketing111@gmail.com>
2022-08-18 11:46:45 -0400

    specified the format of the data

--- src/App6.js
+++ src/App6.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
 import './App.css'
 
 const App6 = () => {
     const { register, handleSubmit } = useForm();
+
+    const schema = yup.object().shape({
+        fullName: yup.string().required(),
+        email: yup.string().email().required(),
+        age: yup.number().positive().integer().min(18).required(),
+        password: yup.string().min(4).max(20).required(),
+        confirmPassword: yup.string().oneOf([yup.ref("password"), null]).required()
+    })
 
     const onSubmit = (data) => {
         console.log(data);
