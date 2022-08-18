e5b8e5d49baa77a0c6923fe9668d70329e295229
Tim La <tlamarketing111@gmail.com>
2022-08-18 11:38:13 -0400

    utlized onSubmit to submit console log

--- src/App6.js
+++ src/App6.js
@@ -3,8 +3,13 @@ import { useForm } from "react-hook-form";
 import './App.css'
 
 const App6 = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = () => {
+        console.log("HELLO WORLD");
+    }
     return (
-        <form style={{ display: "flex", flexDirection: "column", width: "50%" }}>
+        <form style={{ display: "flex", flexDirection: "column", width: "50%" }} onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placeholder="Full Name..." />
             <input type="text" placeholder="Email..." />
             <input type="text" placeholder="Age..." />
