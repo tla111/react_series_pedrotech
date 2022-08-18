f0b942937d2b12b21a220363b7e91990b78f22d0
Tim La <tlamarketing111@gmail.com>
2022-08-18 11:55:28 -0400

    specified error message for fullName

--- src/App6.js
+++ src/App6.js
@@ -6,7 +6,7 @@ import './App.css'
 
 const App6 = () => {
     const schema = yup.object().shape({
-        fullName: yup.string().required(),
+        fullName: yup.string().required("Your full name is required"),
         email: yup.string().email().required(),
         age: yup.number().positive().integer().min(18).required(),
         password: yup.string().min(4).max(20).required(),
