b8b45fba027bdcf3e5238059ab4ae61a1d1e1cce
Tim La <tlamarketing111@gmail.com>
2022-08-24 18:27:21 -0400

    tested out another useToggle hook

--- src/App7.js
+++ src/App7.js
@@ -3,12 +3,16 @@ import './App.css';
 import { useToggle } from './Components/useToggle';
 
 const App7 = () => {
-    const [isVisible, toggle] = useToggle()
+    const [isVisible, toggle] = useToggle();
+    const [isVisible2, toggle2] = useToggle();
 
     return (
         <div className="App">
             <button onClick={toggle}>{isVisible ? "Hide" : "Show"}</button>
             {isVisible && <h1>Hidden Text</h1>}
+
+            <button onClick={toggle2}>{isVisible2 ? "Hide" : "Show"}</button>
+            {isVisible2 && <h1>Hidden Text 2</h1>}
         </div>
     )
 }
