82d4db106e0b6635cf93106556af1c4219a5cad9
Tim La <tlamarketing111@gmail.com>
2022-08-24 18:04:33 -0400

    created app7 for new tutorial

---
+++ src/App7.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App7 = () => {
+    return (
+        <div className="App">App7</div>
+    )
+}
+
+export default App7;
--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import ReactDOM from 'react-dom/client';
 // import App3 from './App3';
 // import App4 from './App4';
 // import App5 from './App5';
-import App6 from './App6';
+// import App6 from './App6';
+import App7 from './App7';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App6 />
+    <App7 />
   </React.StrictMode>
 );
 
