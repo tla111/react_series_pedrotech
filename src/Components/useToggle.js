9ea78778eb28c5cd4da3d37c557324160ce64dca
Tim La <tlamarketing111@gmail.com>
2022-08-24 18:23:45 -0400

    building logic for useToggle hook

--- src/Components/useToggle.js
+++ src/Components/useToggle.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useState } from "react";
 
-const useToggle = () => {
-    return (
-        <div>useToggle</div>
-    )
+export const useToggle = (initialValue = false) => {
+    const [state, setState] = useState(initialValue);
+
+    const toggle = () => {
+        setState((prev) => !prev)
+    }
+
+    return [state, toggle]
 }
-
-export default useToggle
