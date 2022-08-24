cb315f5885e7f6d1936af816d724c19e76333d3e
Tim La <tlamarketing111@gmail.com>
2022-08-24 18:26:09 -0400

    useToggle hook working properly

--- src/App7.js
+++ src/App7.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import { useToggle } from './Components/useToggle';
 
 const App7 = () => {
+    const [isVisible, toggle] = useToggle()
+
     return (
-        <div className="App">App7</div>
+        <div className="App">
+            <button onClick={toggle}>{isVisible ? "Hide" : "Show"}</button>
+            {isVisible && <h1>Hidden Text</h1>}
+        </div>
     )
 }
 
